14e1e77d5d2f1a78e362d6c660fac1040ec187b2
Diego <diego.minchala@outlook.com>
2022-11-18 13:28:15 -0700

    GTA 4

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-let cash=10
-let price=50
+let cash=6000
+let price=100
 
-if (cash > price){
-    console.log (`you paid extra, here's your change ${cash-price}`)
+if (cash>price){
+console.log(`you paid extra, here's your ${cash-price} money back`)
+}
+else if (cash===price){
+    console.log("You r good, enjoy!")
 }
 
-else if(cash===price){
-    console.log ("Enjoy your course!")
-}
-
-else{
-    console.log(`You broke asf, you need ${price-cash} more`)
-
+else {
+    console.log(`Brokie, get your money up, you need ${price-cash} more.`)
 }
