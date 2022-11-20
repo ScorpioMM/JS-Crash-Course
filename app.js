f6d8e97c68c704e0b124249d35b93f83534754f6
Diego <diego.minchala@outlook.com>
2022-11-19 18:42:20 -0700

    jsss

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-let cash=80
-let price=70
-
-if (cash>=70) { console.log( `thank you, heres ur change ${cash-price}`)
+const str = "US KARIM IS THE BEST"
+for (i=0; i<str.length; ++i) {
+    console.log(str[i])
 }
-
