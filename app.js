429b3d7808486eba188a9d1bdf858e727cc813d0
Diego <diego.minchala@outlook.com>
2022-11-22 01:52:02 -0700

    dam

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
-let cash=30
-let price=20
+for (i=1; i>=20; i++) {
+    if (i%3===0 && i%5===0) {
+        console.log (`Frontend Simplified ${i}`)
+    }
+  else if (i%3===0) {
+        console.log (`Frontend ${i}`)
+    }
+  else  if (i%5===0) {
+        console.log (`Simplified ${i}`)
+    }
+else {
+        console.log (`${i} -> ${i}`)
+    }
+}
+ 
+
+let cash=100
+let price=100
+
+if (cash>price) {
+    console.log (`Change ${cash-price}`)
+}
+
+else if (cash===price) {
+    console.log (`Lorem50`)
+}
+
+else {
+    console.log (`not enough, you need ${price-cash} more`)
+}
+
+
+let money=200    
+let moremoney=400
 let isStoreOpen=true
 
-let str = cash>=price && isStoreOpen ? `yes` : `no` 
-console.log(str) 
+let str = money>=moremoney && isStoreOpen ? `Yes` : `No`
+console.log (str)
+
+
+const perra = "UsKarim" 
+for (i=0; i<perra.length; ++i){
+    console.log(perra)
+}
+
+
+function WelcomeToFES (name) {
+    WelcomeToFES (name)
+}
+
+WelcomeToFES (`Karim`)
