cdeb88bdd315054744df758ad47cfece2e4477e4
Diego <diego.minchala@outlook.com>
2022-11-25 09:04:10 -0700

    Forloops

--- app.js
+++ app.js
@@ -1,70 +1,18 @@
-for (i=1; i>=20; i++) {
+for (i=1; i<20; ++i) 
     if (i%3===0 && i%5===0) {
-        console.log (`Frontend Simplified ${i}`)
+        console.log(`${i} -> Frontend Simplified`)
     }
-  else if (i%3===0) {
-        console.log (`Frontend ${i}`)
+
+
+else if (i%3===0 ) {
+        console.log(`${i} -> Frontend `)
     }
-  else  if (i%5===0) {
-        console.log (`Simplified ${i}`)
+else  if (i%5===0) {
+        console.log(`${i} -> Simplified`)
     }
-else {
-        console.log (`${i} -> ${i}`)
-    }
-}
- 
 
-
-
-
-let cash=100
-let price=100
-
-if (cash>price) {
-    console.log (`Change ${cash-price}`)
-}
-
-else if (cash===price) {
-    console.log (`Lorem50`)
-}
 
 else {
-    console.log (`not enough, you need ${price-cash} more`)
-}
-
-
-let money=200    
-let moremoney=400
-let isStoreOpen=true
-
-let str = money>=moremoney && isStoreOpen ? `Yes` : `No`
-console.log (str)
-
-
-const perra = "UsKarim" 
-for (i=0; i<perra.length; ++i){
-    console.log(perra)
-}
-
-
-function WelcomeToFES (name) {
-    WelcomeToFES (name)
-} 
-
-WelcomeToFES (`Karim`)
-
-for (i=1; i>=20; i++) {
-    if (i%3===0 && i%5===0) {
-        console.log (`Frontend Simplified ${i}`)
+        console.log(`${i} -> ${i}`)
     }
-  else if (i%3===0) {
-        console.log (`Frontend ${i}`)
-    }
-  else  if (i%5===0) {
-        console.log (`Simplified ${i}`)
-    }
-else {
-        console.log (`${i} -> ${i}`)
-    }
-}
- 
+
