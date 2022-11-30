29c0b9a5ab3423019fe2d93775c40cb62406ae33
Diego <diego.minchala@outlook.com>
2022-11-29 19:18:44 -0700

    arrays

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-let arr = [20, 30, 40,50,100] 
-
-let NewArr = arr.filter((element) => {
-    console.log(element)
-        if (element < 50){
-            return true; 
-
-        }
-})
-console.log (NewArr)
+const Weather = (celsious) => {}
+return celsious * 1.8 + 32 
