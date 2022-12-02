701677b72002c3e293f92ecf39b63ed438443f9f
Diego <diego.minchala@outlook.com>
2022-12-01 17:06:28 -0700

    promises

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-let grades = [`A+` ,`A`,`FAIL`] 
-
-let goodGrades = []
-
-for (i = 0; i < grades.length; ++i){
-    if (grades[i] !== `FAIL`){ 
-        goodGrades.push(grades[i])
-    }
+const statusRef = document.querySelector(".status")
+const videoRef = document.querySelector(".video")
+function getSubcriptionStatus(){
+    return new Promise ((resolve,reject) => {
+setTimeout (() => {
+    resolve ("undefined")
+} , 2000);
+    });
 }
 
-console.log(goodGrades)
+
+function getVideo (subscriptionStatus){
+
+    return new Promise ((resolve, reject) => {
+if (subscriptionStatus === "VIP") {
+    resolve ("show video")
+}
+else if (subscriptionStatus === "FREE"){
+    resolve ("show trailer")
+}
+else {
+    reject ("no video")
+}
+    })
+}
+
+
+async function main () {
+const status = await getSubcriptionStatus ();
+statusRef.innerHTML = status;
+try {
+    console.log(await getVideo(status))
+}
+catch (e) {
+    console.log(e)
+    videoRef.innerHTML = e;
+}
+
+}
+
+main (); 
