bab6efd2b5bbab1526709f586846b47a5f1fe999
Diego <diego.minchala@outlook.com>
2022-12-07 03:42:56 -0700

    loops

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 let str = (`welcome To FES Karim`) 
-for (i = 0; i < str.length; i++){
-    console.log(str[i])
+for (i = 0; i < 21 ; i++){
+if(i % 3 === 0 & i % 5 ===0) {
+    console.log(`${i} -> Frontend Simplified`)
+}   
+
+else if (i % 3 === 0) {
+    console.log(`${i} -> Frontend`)
+}
+
+else if (i % 5 === 0) {
+    console.log(`${i} -> Simplified`)
+}
+
+else {
+    console.log(`${i} -> ${i}`)
+}
+
+
 }
