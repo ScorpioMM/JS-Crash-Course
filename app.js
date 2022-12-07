b0dcba200a6db6a7f57bb755c37514a42563e4d5
Diego <diego.minchala@outlook.com>
2022-12-06 19:44:28 -0700

    Doing Functions again

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-const statusRef = document.querySelector(".status")
-const videoRef = document.querySelector(".video")
-function getSubcriptionStatus(){
-    return new Promise ((resolve,reject) => {
-setTimeout (() => {
-    resolve ("undefined")
-} , 2000);
-    });
+function Welcomeback (name) {
+    console.log(`Welcomeback To FES, ${name}`)
 }
 
-
-function getVideo (subscriptionStatus){
-
-    return new Promise ((resolve, reject) => {
-if (subscriptionStatus === "VIP") {
-    resolve ("show video")
-}
-else if (subscriptionStatus === "FREE"){
-    resolve ("show trailer")
-}
-else {
-    reject ("no video")
-}
-    })
-}
+Welcomeback (`Diego`); 
