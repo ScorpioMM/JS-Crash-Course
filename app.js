e86a4efe0d0aa9b9aec5ef865faee9344cc9abe7
Diego <diego.minchala@outlook.com>
2022-12-10 16:06:50 -0700

    arrays, they kinda easy ngl

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-let arr = [20, 30, 40, 50, 100]
+let grades = [`A+`, `A`, `FAIL`]
 
-let newArr = arr.filter((element) => {
-console.log(element)
-return true;
-})
+let gooGrades = []
+
+for (i=0; i < grades.length; ++i){
+    if (grades[i] !== `FAIL`)
+    gooGrades.push(grades[i]);
+}
+
+console.log(gooGrades)
 
 
-arr.push(400);
+let arr = [20,30, 40,50, 100]
 
-console.log(arr)
+let newArr = arr.filter(element => element < 50)
+
+console.log(newArr)
+
+
+
+
+let dogs = [20, 30, 40, 90]
+
+console.log(dogs[0])
+
+console.log(dogs [dogs.length-1])
+
+dogs.push(100);
+
+console.log(dogs)
