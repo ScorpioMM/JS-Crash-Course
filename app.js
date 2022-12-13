1ea96f288a866752a270660c913f0c3b5bb26912
Diego <diego.minchala@outlook.com>
2022-12-13 16:12:45 -0700

    js

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
-let dogs = [20,30,40,50,100]
-console.log(dogs[0])
+// Basic arrays 
 
-console.log(dogs[dogs.length-1])
+let beats = [30,40,50,100]
+console.log(beats[1])
+console.log(beats[beats.length-1])
+beats.push(3000);
 
-dogs.push(300);
-console.log(dogs)
+console.log(beats)
+// arrays.filter 
 
+let arr = [40,50,60,70,80]
 
-// WE using arrays.filter now
-
-let arr = [20, 30, 40, 50, 100]
-
-let newArr = arr.filter((element)=>{
-    if (element < 50){
-        return true;
-    }
+let newArr = arr.filter ((element) => {
+if (element < 70)
+return true;
 })
+
 console.log(newArr)
 
+// arrays.filter short way 
+let arrays = [20,40,50,60,100]
+let newArrays = arrays.filter (element => element < 60)
 
-let newKarim = [1,2,3,4,5,10]
+console.log(newArrays)
+
+// using .map (() => {})
+
+let newKarim = [1,5,10]
 
 let karim = newKarim.map ((element) => {
     console.log(element)
-    return `doggie xd`;
+    return `newMoney`;
 })
 console.log(karim)
+
+// strings
+
+let youtube = 200
+let subscription = 200
+
+if (youtube < subscription) {
+    console.log(`${youtube-subscription}`)
+}
+else if (youtube === subscription) {
+    console.log(`thank you :)`)
+}
+
+
+// 
+
+let cash = [1,5,10,3];
+
+let cents = [];
+
+for (i = 0; i < cash.length; ++i){
+    cents.push(cash[i] * 100);
+}
+
+console.log(cents);
+
