0bcef95f2b6ff69d9d7a33c5f48dc6e57a9c725f
Diego <diego.minchala@outlook.com>
2022-12-13 16:53:33 -0700

    objects

--- app.js
+++ app.js
@@ -1,60 +1,34 @@
-// Basic arrays 
-
-let beats = [30,40,50,100]
-console.log(beats[1])
-console.log(beats[beats.length-1])
-beats.push(3000);
-
-console.log(beats)
-// arrays.filter 
-
-let arr = [40,50,60,70,80]
-
-let newArr = arr.filter ((element) => {
-if (element < 70)
-return true;
-})
-
-console.log(newArr)
-
-// arrays.filter short way 
-let arrays = [20,40,50,60,100]
-let newArrays = arrays.filter (element => element < 60)
-
-console.log(newArrays)
-
-// using .map (() => {})
-
-let newKarim = [1,5,10]
-
-let karim = newKarim.map ((element) => {
-    console.log(element)
-    return `newMoney`;
-})
-console.log(karim)
-
-// strings
-
-let youtube = 200
-let subscription = 200
-
-if (youtube < subscription) {
-    console.log(`${youtube-subscription}`)
-}
-else if (youtube === subscription) {
-    console.log(`thank you :)`)
+let user = [{
+username: `Karim` ,
+email: `karim@gmail.com`,
+password: `perro1213`,
+subscriptionStatus: `VIP`,
+discordID: `USKarim`,
+lessonsCompleted: [0,1]
+},
+{
+    username: `iphone` ,
+    email: `phone@gmail.com`,
+    password: `dog1213`,
+    subscriptionStatus: `VIP`,
+    discordID: `iphone`,
+    lessonsCompleted: [0,1,2,3]
+},
+{
+    username: `zen` ,
+    email: `zen@gmail.com`,
+    password: `zen`,
+    subscriptionStatus: `VIP`,
+    discordID: `zen`,
+    lessonsCompleted: [0,1,2,3]
+},
+];
+function login(email, password){
+    for(i=0; i < user.length; ++i ) {
+        if (user[i].email === email)
+        console.log(user[i]);
+    }
+console.log(user[i]);
 }
 
-
-// 
-
-let cash = [1,5,10,3];
-
-let cents = [];
-
-for (i = 0; i < cash.length; ++i){
-    cents.push(cash[i] * 100);
-}
-
-console.log(cents);
-
+login(`karim@gmail.com` , `perro1213`);
