6cab8366d1baf9fe80380374a0c66d53abf51969
Diego <diego.minchala@outlook.com>
2022-12-16 21:34:39 -0700

    arrays

--- app.js
+++ app.js
@@ -1,62 +1,43 @@
-let users = [{
-    username: "diego" ,
-    email: "diego@gmail.com" ,
-    password: "diego1213" ,
-    status: "VIP" ,
-    discordID: "011diego" ,
-    lessonsCompleted: [0,1] ,
-},
-{
-    username: "karim" ,
-    email: "karim@gmail.com" ,
-    password: "karim11" ,
-    status: "VIP" ,
-    discordID: "karim01" ,
-    lessonsCompleted: [0,1] ,
-},
-{
-    username: "diego" ,
-    email: "diego@gmail.com" ,
-    password: "diego1213" ,
-    status: "VIP" ,
-    discordID: "011diego" ,
-    lessonsCompleted: [0,1] ,
-}
-]
-
-function login (email,password) {
-
-for (i = 0; i < users.length; i++) {
-
-if(users[i].email === email){
-    if (users[i].password === password){
-        console.log(`let him in`)
-    }
-else {console.log(`NO NIGGA`)}
-
-return;
-}
-}
-console.log(`couldnt find an email`)
+function weather (celsious) {
+ let fahrenheit = celsious * 1.8 + 32
+ return fahrenheit;
 }
 
-login (`karim@gmail.com` , `karim11`)
+console.log(weather(50)); 
 
 
+// shorter way 
 
-// register 
 
-function register (user){
-    users.push(user)
+function weathera (celsiouz) {
+    return celsiouz * 1.8 + 32
 }
 
-register ({
-    username: `zen`,
-    email: "zen@gmail.com" ,
-    password: "zen" ,
-    status: "VIP" ,
-    discordID: "01zen" ,
-    lessonsCompleted: [0,1] ,
+console.log(weathera(30))
+
+
+/// arrays 
+
+let karim = [20, 30, 40, 50, 100] 
+
+console.log(karim[0])
+
+console.log(karim[karim.length - 1]) 
+
+karim.push(100);
+
+console.log(karim)
+
+
+// arrays . filter
+
+let grades = [`A+`,`A`, `FAIL`]
+
+let goodGrades = grades.filter((element) => {
+    if (element !== `FAIL`)
+    return true;
 })
 
-console.log(users);
+console.log(goodGrades)
+
+
