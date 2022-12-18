78d66b52db27a7549be70e5036523551ebf3c890
Diego <diego.minchala@outlook.com>
2022-12-18 13:49:32 -0700

    DOM

--- app.js
+++ app.js
@@ -1,120 +1,18 @@
-function weather (celsious) {
- let fahrenheit = celsious * 1.8 + 32
- return fahrenheit;
+// first way
+// Its always used with a hash and dots, haash when u got IDS and . when u got classes
+// console.log(document.querySelector('h1'))
+
+// Second way, no ID
+
+// how to change the HTML on anything, used the method below, used += to ADD into the server
+
+document.querySelector('.title').innerHTML = 'Frontend Simplified'
+
+// change css, from color to font size. this document.querySelector helps u change anything. 
+
+document.querySelector('.title').style.fontSize = '38px'
+
+function clickme (){
+    document.querySelector(".title").style.color = 'red'
+    console.log('clicked')
 }
-
-console.log(weather(50)); 
-
-
-// shorter way 
-
-
-function weathera (celsiouz) {
-    return celsiouz * 1.8 + 32
-}
-
-console.log(weathera(30))
-
-
-/// arrays 
-
-let karim = [20, 30, 40, 50, 100] 
-
-console.log(karim[0])
-
-console.log(karim[karim.length - 1]) 
-
-karim.push(100);
-
-console.log(karim)
-
-
-// arrays . filter
-
-let grades = [`A+`,`A`, `FAIL`]
-
-let goodGrades = grades.filter((element) => {
-    if (element !== `FAIL`)
-    return true;
-})
-
-console.log(goodGrades)
-
-// 
-
-let cash = 500  
-let price = 200
-
-    if (cash > price) {
-        console.log(`${cash-price} change`)
-    }
-
-    else if (cash === price) {
-        console.log(`let him in, thank you`)
-    }
-
-    else {console.log(`${price - cash} you need`)
-     }
-
-// login 
-
-let  users = [{
-username: `karim`,
-email: `karim@gmail.com`,
-password: `karim12` ,
-subscriptionStatus: `VIP` ,
-discordID: `karim01` , 
-lessonsCompleted: [0, 1]
-},
-{
-username: `karim`,
-email: `karim@gmail.com`,
-password: `karim12` ,
-subscriptionStatus: `VIP` ,
-discordID: `karim01` , 
-lessonsCompleted: [0, 1]
-} ,
-{
-username: `karim`,
-email: `karim@gmail.com`,
-password: `karim12` ,
-subscriptionStatus: `VIP` ,
-discordID: `karim01` , 
-lessonsCompleted: [0, 1]
-}
-] 
-
-function login (email,password) {
-for (i=0; i < users.length; i++) {
-    if (users[i].email === email) {
-        if (users[i].password === password)
-     console.log(`let him in`)   
-    }
-    else {console.log(`wrong password`)}        
-        
-return;
-
-}
-
-}
-
-login (`karim@gmail.com` , `karim12`)
-
-
-let register = {
-    username: `zen`,
-    email: `zen@gmail.com`,
-    password: `ze1` ,
-    subscriptionStatus: `VIP` ,
-    discordID: `zen11` , 
-    lessonsCompleted: [0, 3]
-}
-
-
-function register (user){
-    users.push(user)
-}
-
-
-
-
