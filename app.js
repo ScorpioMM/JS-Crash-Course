54f297113bd4aaa4e59b4c02a9f2386187c1ca01
Diego <diego.minchala@outlook.com>
2022-12-18 17:51:06 -0700

    promises

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-// first way
-// Its always used with a hash and dots, haash when u got IDS and . when u got classes
-// console.log(document.querySelector('h1'))
+// fetch ("https://jsonplaceholder.typicode.com/users/1"))
 
-// Second way, no ID
+// THEN or we could use something called Await/Async
 
-// how to change the HTML on anything, used the method below, used += to ADD into the server
+// THEN
 
-// document.querySelector('.title').innerHTML = 'Frontend Simplified'
-
-// change css, from color to font size. this document.querySelector helps u change anything. 
-
-// document.querySelector('.title').style.fontSize = '38px'
-
-// function clickme (){
-  //  document.querySelector(".title").style.color = 'red'
- //   console.log('clicked')
-// }
+fetch ("https://jsonplaceholder.typicode.com/users/1").then(response => {
+console.log(response.json())
+response.json().then(data => {
+    console.log(data)
+})
+})
