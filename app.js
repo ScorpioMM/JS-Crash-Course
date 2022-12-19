df4901820f5dc90092a006a612a6429e91304982
Diego <diego.minchala@outlook.com>
2022-12-19 01:04:25 -0700

    doms

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-// fetch ("https://jsonplaceholder.typicode.com/users/1"))
+function getSubscriptionStatus () {
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("VIP")
+        
+    }, 2000);
+})
 
-const emailRef = document.querySelector(".email")
-// THEN or we could use something called Await/Async
-
-// THEN
-
-//fetch (("https://jsonplaceholder.typicode.com/users/1"))
-//.then((response) => {
-  //  return response.json();
-//})
-//.then ((data) => {
-//console.log(data);
-//emailRef.innerHTML = data.email
-// });
-
-// AWAIT AND ASYNC
-async function main () {
- const response =   await fetch ("https://jsonplaceholder.typicode.com/users/1")
- const data = await response.json()
-console.log(data)
-emailRef.innerHTML = data.email
 }
 
-main ()
+// .then
+getSubscriptionStatus().then (response => console.log(response))
+
+
+// async and await 
+async function main() {
+    console.log( await getSubscriptionStatus());
+
+}
+
+main(); 
