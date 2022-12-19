d0c8db035c3921a2fd76feca51a74be8ca1e0c51
Diego <diego.minchala@outlook.com>
2022-12-19 02:38:41 -0700

    promises

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-function getSubscriptionStatus () {
-return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("VIP")
-        
-    }, 2000);
+const emailRef = document.querySelector ("email")
+
+fetch (("https://jsonplaceholder.typicode.com/users/1"))
+.then ((response) => {
+    return response.json ();
 })
 
-}
+    
+.then ((data) => {
+console.log(data);
+    
+emailRef.innerHTML = data.email });
 
-// .then
-getSubscriptionStatus().then (response => console.log(response))
-
-
-// async and await 
-async function main() {
-    console.log( await getSubscriptionStatus());
-
-}
-
-main(); 
