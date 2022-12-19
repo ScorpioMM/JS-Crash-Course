fe026e539668cb18cb1e60999aaf16a547c0a49e
Diego <diego.minchala@outlook.com>
2022-12-18 20:33:52 -0700

    promises x2

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 // fetch ("https://jsonplaceholder.typicode.com/users/1"))
 
 const emailRef = document.querySelector(".email")
-console.log(emailRef)
 // THEN or we could use something called Await/Async
 
 // THEN
 
-fetch ("https://jsonplaceholder.typicode.com/users/1").then(response => {
-response.json().then(data => {
-    console.log(data)
-    emailRef.innerHTML = data.email;
-})
-})
+//fetch (("https://jsonplaceholder.typicode.com/users/1"))
+//.then((response) => {
+  //  return response.json();
+//})
+//.then ((data) => {
+//console.log(data);
+//emailRef.innerHTML = data.email
+// });
+
+// AWAIT AND ASYNC
+async function main () {
+ const response =   await fetch ("https://jsonplaceholder.typicode.com/users/1")
+ const data = await response.json()
+console.log(data)
+emailRef.innerHTML = data.email
+}
+
+main ()
