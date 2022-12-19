f6b98f480bab86421ea668df785f6305f805100e
Diego <diego.minchala@outlook.com>
2022-12-18 18:05:55 -0700

    fecths

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // fetch ("https://jsonplaceholder.typicode.com/users/1"))
 
+const emailRef = document.querySelector(".email")
+console.log(emailRef)
 // THEN or we could use something called Await/Async
 
 // THEN
 
 fetch ("https://jsonplaceholder.typicode.com/users/1").then(response => {
-console.log(response.json())
 response.json().then(data => {
     console.log(data)
+    emailRef.innerHTML = data.email;
 })
 })
