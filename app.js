204fcd2be6efa439fa9839e7436f4c2f5d52e8b3
Diego <diego.minchala@outlook.com>
2022-12-21 19:56:40 -0700

    promises

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const emailRef = document.querySelector ("email")
+const emailRef = document.querySelector (".email");
 fetch (("https://jsonplaceholder.typicode.com/users/1"))
 .then ((response) => {
     return response.json ();
 })
-
+.then ((data) => {
+console.log(data);
+emailRef.innerHTML = email.data
+});
 // 
 
 let grades = ['A+' , 'A' , 'F']
