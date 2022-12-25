8c0fc9a02876c457a668e4aa94b7edff35867cab
Diego <diego.minchala@outlook.com>
2022-12-24 18:44:43 -0700

    arrays

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// Functions lets you "show" what you wanna show on the website or console
+
 function arr (username) {
     return username
 }
 console.log(`karim`)
+
+// Theres two ways to do this ^^
 
 const newArr = (password) => {
 return password
@@ -9,9 +13,45 @@ return password
 
 console.log(`perro1213`)
 
+// arrays lets u choose one (or more?) elements.
+
 let array = [20, 30, 40, 50, 100] 
 console.log(array[2])
+
+
+// runs every element as truth.
 
 let newArray = array.filter ((num1) => {
     console.log(num1)
 })
+
+// more arrays examples :) 
+
+let dogs = [0, 1, 2, 3, 4, 5]
+console.log(dogs[3])
+
+console.log(dogs[dogs.length-1])
+
+// push
+
+dogs.push (555); 
+console.log (dogs)
+
+// more filters problems and ofc, solutions 
+
+let arrNew = [20, 40, 50, 70,  60, 80] 
+
+
+//let arrNewT = arrNew.filter ((element) => {
+//    if (element < 50 ) {
+  //      return true;
+    // }
+// })
+
+// console.log(arrNewT) 
+
+//Second and cleaner way to run this problem
+
+let arrNewT = arrNew.filter (element => element < 50)
+console.log(arrNewT)
+
