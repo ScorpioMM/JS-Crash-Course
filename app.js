12937e4cb9386b58878fc97c0d8d063f19071644
Diego <diego.minchala@outlook.com>
2022-12-24 18:13:58 -0700

    foor loops review

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
-const emailRef = document.querySelector (".email");
-fetch (("https://jsonplaceholder.typicode.com/users/1"))
-.then ((response) => {
-    return response.json ();
-})
-.then ((data) => {
-console.log(data);
-emailRef.innerHTML = email.data
-});
-
-// 
-
-let grades = ['A+' , 'A' , 'F']
-let goodGrades = []
-
-for (i = 0; i < grades.length; ++i) {
-    if (grades[i] !== 'F') 
-goodGrades.push (grades[i]);
+let str = `Frontend Simplified` 
+for (i = 0; i < str.length; ++i) {
+if (i % 3 === 0 && i % 5 ===0) {
+    console.log(`${i} --> FRONTEND SIMPLIFIED`)
 }
 
-console.log(goodGrades)
+else if (i % 3 === 0) {
+    console.log(`${i} ---> Frontend`)
+}
 
-//
+else if (i % 5 === 0) {
+    console.log(`${i} ---> Simplified`)
+}
 
-let arr = [1, 2]
-let newaRR = arr.map ((element) => {
-    console.log(element)
-    return `pene`;
-})
+else {console.log(`${i} --> ${i}`)}
 
-console.log(newaRR)
+}
+
