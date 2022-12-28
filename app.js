42bcb86484c34f68ba00e7851a459e928c49a364
Diego <diego.minchala@outlook.com>
2022-12-28 15:44:10 -0700

    js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function arrLenght (arr) {
+function arrLenght(arr) {
 
 return arr[arrLenght-1];
 
@@ -6,3 +6,10 @@ return arr[arrLenght-1];
 
 console.log(arrLenght([null, 5, false]));
 
+
+
+function karim (num1, num2) {
+    return num1 + num2
+}
+
+console.log(karim(5,7))
