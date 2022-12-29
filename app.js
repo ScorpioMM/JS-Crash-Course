90f383b2dec234c34da21f8e4481cd7f3ddd77fc
Diego <diego.minchala@outlook.com>
2022-12-28 19:52:12 -0700

    js

--- app.js
+++ app.js
@@ -6,16 +6,15 @@
 
 //console.log(arrLength([1,2,4,8,5]));
 
+function karim (loggedIn, subscribe) {
 
+    if (loggedIn === 'logged_in' && subscribe === 'subscribe'){
+        return true;
+    }
+    else {
+        return false;
+    }
 
-function karim (num1) {
-
-if (num1 != 0) {
-    return true;
 }
-else {
-    return false;
-}
-}
 
-console.log(karim(''))
+console.log(karim('logged_n' , "subscribe"))
