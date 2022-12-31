eda943dfb875943dd56735796fbe20e378348997
Diego <diego.minchala@outlook.com>
2022-12-30 21:41:37 -0700

    memories sintax

--- app.js
+++ app.js
@@ -9,3 +9,25 @@ function diego (num1, num2){
 }
 
 console.log(diego(5, 10))
+
+
+let dogs = [20, 30, 40, 50, 60]
+
+console.log(dogs); 
+
+console.log(dogs.length)
+
+dogs.push(300);
+
+console.log(dogs)
+
+
+let arr = [ 40, 50, 100] 
+
+let newArr = arr.filter((element) => {
+    if (element < 50) {
+        return true;
+    }
+})
+
+console.log(newArr)
