648704021a016bb7e02a57480950802f3a64917d
Diego <diego.minchala@outlook.com>
2022-12-31 19:18:02 -0700

    lmao

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-//function karim(arr) {
-//return arr.length
-//}
+function karim(arr) {
+return arr.length
+}
 
-//console.log(karim([5, 5, 6, 7]))
+console.log(karim([5, 5, 6, 7]))
 
-//function diego (num1, num2){
-  //  return num1 + num2
-//}
+function diego (num1, num2){
+    return num1 + num2
+}
 
-//console.log(diego(5, 10))
-
-
-//let dogs = [20, 30, 40, 50, 60]
-
-//console.log(dogs); 
-
-//console.log(dogs.length)
-
-//dogs.push(300);
-
-//console.log(dogs)
+console.log(diego(5, 10))
 
 
-//let arr = [ 40, 50, 100] 
+let dogs = [20, 30, 40, 50, 60]
 
-//let newArr = arr.filter((element) => {
-  //  if (element < 50) {
-   //     return true;
-    //}
-//})
+console.log(dogs); 
 
-//console.log(newArr)
+console.log(dogs.length)
+
+dogs.push(300);
+
+console.log(dogs)
+
+
+let arr = [ 40, 50, 100] 
+
+let newArr = arr.filter((element) => {
+    if (element < 50) {
+        return true;
+    }
+})
+
+console.log(newArr)
 
 let nigga = [50, 60]
 
