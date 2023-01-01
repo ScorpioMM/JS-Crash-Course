6eb335f5a5f55b7dd2be4a6b5a19f9585b341440
Diego <diego.minchala@outlook.com>
2023-01-01 00:12:26 -0700

    practices

--- app.js
+++ app.js
@@ -1,52 +1,7 @@
-function karim(arr) {
-return arr.length
+function diego (length, width) {
+
+return 2 * ( length + width )
+    
 }
 
-console.log(karim([5, 5, 6, 7]))
-
-function diego (num1, num2){
-    return num1 + num2
-}
-
-console.log(diego(5, 10))
-
-
-let dogs = [20, 30, 40, 50, 60]
-
-console.log(dogs); 
-
-console.log(dogs.length)
-
-dogs.push(300);
-
-console.log(dogs)
-
-
-let arr = [ 40, 50, 100] 
-
-let newArr = arr.filter((element) => {
-    if (element < 50) {
-        return true;
-    }
-})
-
-console.log(newArr)
-
-let nigga = [50, 60]
-
-let newNigga = nigga.map ((elem) => {
-    console.log(elem) 
-    return `SKEREEEE`;
-})
-
-console.log(newNigga)
-
-
-let dollars = [1, 5, 7, 8]
-let cents = []
-
-for (let i = 0; i < dollars.length; i++) {
-    cents.push(dollars[i] * 100)
-}
-
-console.log(cents)
+console.log(diego(2, 9))
