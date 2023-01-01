b232ba514435c5ec956606429fae2f0e0bdd7389
Diego <diego.minchala@outlook.com>
2022-12-31 17:24:29 -0700

    remember

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function karim(arr) {
-return arr.length
-}
+//function karim(arr) {
+//return arr.length
+//}
 
-console.log(karim([5, 5, 6, 7]))
+//console.log(karim([5, 5, 6, 7]))
 
-function diego (num1, num2){
-    return num1 + num2
-}
+//function diego (num1, num2){
+  //  return num1 + num2
+//}
 
-console.log(diego(5, 10))
+//console.log(diego(5, 10))
 
 
 let dogs = [20, 30, 40, 50, 60]
