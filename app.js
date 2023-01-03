ca62a5613f1c6e9856e198a40a81eb80e1fd8d9f
Diego <diego.minchala@outlook.com>
2023-01-03 12:57:57 -0700

    remembering

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-function diego (num1) {
-    return num1 < false 
-}
-    
+// Foor Loops
 
-console.log(diego(false))
+let diego = ('Frontend Simplified') 
+for (let i = 0; i < diego.length; ++i){
 
-function karim (num4, num3) {
-    return num4 + num3
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log(`${i} -- > FrontendSimplified`)
+    }
+else if ( i % 3 === 0){
+    console.log(`${i} --> Frontend`)
 }
-console.log(karim(10, 10))
+else if ( i % 5 === 0){
+    console.log(`${i} --> Simplified`)
+}
+
+else {
+    console.log(`${i} --> ${i}`)
+}
+}
+
+// Functions
+
+function karim (num1, num2) {
+    return num1 + num2
+}
+
+console.log(karim(10, 20))
+
+//Arrays
+
+let arr = ['A+' , 'A', 'F']
+let arrNew = []
+for (let i = 0; i < arr.length; ++i){
+    if (arr[i] !== 'F')
+    arrNew.push(arr[i]);
+
+}
+
+console.log(arrNew)
+
+// 
