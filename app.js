02f9ae156134f33a9dcdc7a9d60ef517518847dd
Diego <diego.minchala@outlook.com>
2023-01-07 17:55:10 -0700

    dam

--- app.js
+++ app.js
@@ -1,29 +1,66 @@
+// TIME //
+
+
+//function time(seconds){
+//    let timeMinutes = Math.floor(seconds / 60)
+//    let timeSeconds = seconds % 60
+//    if(timeMinutes.toString().length === 1){
+//        timeMinutes = '0' + timeMinutes
+//    }
+//    return timeMinutes + ':' + timeSeconds
+//}
+//console.log(time(500))
+
+
+
+
 // When doing sums & trynna get the biggest (smallest ? ) we always use a new variable. But on the second option, we use the same name, just different number 
-function time(seconds){
-let timeMinutes = Math.floor(seconds / 60)
-let timeSeconds = seconds % 60
-if(timeMinutes.toString().length === 1){
-    timeMinutes = '0' + timeMinutes
-}
-return timeMinutes + ':' + timeSeconds
-}
-console.log(time(500))
+
+
+//function diego (num1){
+
+ //   let sum = 0; 
+ //   for (let i = 0; i <num1.length; ++i){
+ //       sum = sum + num1[0];
+//    }
+ //   return sum;
+    
+ //   }
+    
+ //   console.log(diego([,2,4,4,2,6]))
+
+
+// SHIT TEST, IN THEIR DNA, MAN. YOU GOTTA KNOW THEY ARE ALWAYS TESTING YOU, PLEASE, PLEASE DONT FAIL THEM OR AT LEAST BE AWARE OF THEM.    
 // You are failing all of them, u r supposed to past those little tests. tienes que ser unmune, literalmente, menos emocional 
+    
+
+
+
+// MONEY, I LOVE YOU SO MUCH, MAN, IM SORRY FOR LEAVING YOU. YOU KNOW I WAS TRYING TO GET EVEN MORE OF YOU BUT I DIDNT KNOW HOW TO MAKE THAT WISH BETTER, THEREFORE I ENDED UP TAKING REALLY BAD DECISIONS, MONETEARALY SPEAKING.
+// I STILL THANK YOU A LOT THO BECAUSE SOMEHOW I ALWAYS FIND A WAY BUT WITH THE SAME RESULTS, I WANNA HAVE YOU FOREVER, TIL THE MY WHOLE BLOODLINE DISAPEARS, BUT HOW CAN I MAKE THIS A REALITY ? 
+// MENTORS ALWAYS START OFF BY SAYING, "THANK THE MONEY THAT YOU USE BECAUSE IT GOES WHERE IT NEEDS TO BE" SOMETHING LIKE THAT. BUT ANYWAYS, HOW?
+// Being THANKFUL, FEEL HOW U USED TO FEEL... GOING TO WORK, EVERYDAY, EVEN THO YOU HATED IT YOU STILL DID... AND YOU WERE GOING TO SCHOOL AND YOU HAD A GIRL!! YOU WERE DOING EVERYTHING, ONLY IF..
+// I COULD START OFF FROM THERE BUT WITH ALL MY IMPROVEMENTS AND LESSONS THAT I HAVE TODAY.
+// SO I AND MONEY COULD FORMALLY GENERATE REAL WHEALTH.
+// YOU NEED A PLAN..
+
 
 
 // GRACIAS DINERO :) . Everytime you use it, no matter on what, be thankful.
 
 // Start making better decisions, first for yourself and then the group around you, guide
 
-// La del 1
-function diego (num1){
 
-let sum = 0; 
-for (let i = 0; i <num1.length; ++i){
-    sum = sum + num1[0];
+function karim (arr){
+let max = arr[0];
+
+for(let i = 0; i < arr.length; ++i){
+if (arr[i] > max){
+    max = arr[i]
 }
-return sum;
-
+    }
+    return max;
 }
 
-console.log(diego([,2,4,4,2,6]))
+console.log(karim([200,-111,500]))
+
