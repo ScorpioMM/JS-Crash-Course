ea216ab3f9a4716be8c0a8cfabdbfcb195ea6999
Diego <diego.minchala@outlook.com>
2023-01-07 17:27:56 -0700

    how

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-//function calcTime (seconds){
-//let timerMinutes = Math.floor(seconds / 60);    
-//let timerSeconds = seconds % 60;
-
-//if(timerMinutes.toString().length === 1){
-//timerMinutes = '0' + timerMinutes
-//}
-
-//return timerMinutes + ':' + timerSeconds;
-
-//}
-
-//console.log(calcTime(70))
-
 // When doing sums & trynna get the biggest (smallest ? ) we always use a new variable. But on the second option, we use the same name, just different number 
-
-
-function diego (elem1) {
-   let sum = 0;
-   for (let i = 0; i < elem1.length; ++i){
-    if (sum + elem1[0]){
-        sum = sum + elem1[0]
-    }
-   }
-   return sum;
+function time(seconds){
+let timeMinutes = Math.floor(seconds / 60)
+let timeSeconds = seconds % 60
+if(timeMinutes.toString().length === 1){
+    timeMinutes = '0' + timeMinutes
 }
-console.log(diego([3,3,3]))
+return timeMinutes + ':' + timeSeconds
+}
+console.log(time(500))
+// You are failing all of them, u r supposed to past those little tests. tienes que ser unmune, literalmente, menos emocional 
+
+
+// GRACIAS DINERO :) . Everytime you use it, no matter on what, be thankful.
+
+// Start making better decisions, first for yourself and then the group around you, guide
+
+// La del 1
+function diego (num1){
+
+let sum = 0; 
+for (let i = 0; i <num1.length; ++i){
+    sum = sum + num1[0];
+}
+return sum;
+
+}
+
+console.log(diego([,2,4,4,2,6]))
