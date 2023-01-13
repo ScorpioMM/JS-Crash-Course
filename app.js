d91b88aba152195db38038ac90a8600c7143738d
Diego <diego.minchala@outlook.com>
2023-01-13 11:07:18 -0700

    javasc

--- app.js
+++ app.js
@@ -17,17 +17,17 @@
 // When doing sums & trynna get the biggest (smallest ? ) we always use a new variable. But on the second option, we use the same name, just different number 
 
 
-//function diego (num1){
+function diego (num1){
 
-//   let sum = 0; 
-//   for (let i = 0; i <num1.length; ++i){
-//       sum = sum + num1[0];
-//    }
-//   return sum;
+let sum = 0; 
+for (let i = 0; i <num1.length; ++i){
+sum = sum + num1[0];
+}
+return sum;
     
-//   }
+}
     
-//   console.log(diego([,2,4,4,2,6]))
+  console.log(diego([,2,4,4,2,6]))
 
 
 // SHIT TEST, IN THEIR DNA, MAN. YOU GOTTA KNOW THEY ARE ALWAYS TESTING YOU, PLEASE, PLEASE DONT FAIL THEM OR AT LEAST BE AWARE OF THEM.    
