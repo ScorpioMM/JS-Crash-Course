2a743c3a0401a7e746df17a943b79e72a3562340
Diego <diego.minchala@outlook.com>
2023-02-25 13:30:31 -0700

    Yessir

--- app.js
+++ app.js
@@ -1,162 +1,17 @@
-// TIME //
+//Return the sum of giving numbers
 
-//function time(seconds){
-//let timeMinutes = Math.floor(seconds / 60)
-//let timeSeconds = seconds % 60
-//if(timeMinutes.toString().length === 1){
-//timeMinutes = '0' + timeMinutes
-//}
-//return timeMinutes + ':' + timeSeconds
-//}
-//console.log(time(500))
-
-
-
-// When doing sums & trynna get the biggest (smallest ? ) we always use a new variable. But on the second option, we use the same name, just different number 
-
-
-function diego (num1){
-
-let sum = 0; 
-for (let i = 0; i <num1.length; ++i){
-sum = sum + num1[0];
-}
-return sum;
-    
-}
-    
-  console.log(diego([2,4,4,2,6]))
-
-// SHIT TEST, IN THEIR DNA, MAN. YOU GOTTA KNOW THEY ARE ALWAYS TESTING YOU, PLEASE, PLEASE DONT FAIL THEM OR AT LEAST BE AWARE OF THEM.    
-// You are failing all of them, u r supposed to past those little tests. tienes que ser unmune, literalmente, menos emocional 
-    
-
-
-// MONEY, I LOVE YOU SO MUCH, MAN, IM SORRY FOR LEAVING YOU. YOU KNOW I WAS TRYING TO GET EVEN MORE OF YOU BUT I DIDNT KNOW HOW TO MAKE THAT WISH BETTER, THEREFORE I ENDED UP TAKING REALLY BAD DECISIONS, MONETEARALY SPEAKING.
-// I STILL THANK YOU A LOT THO BECAUSE SOMEHOW I ALWAYS FIND A WAY BUT WITH THE SAME RESULTS, I WANNA KEEP YOU AROUND FOREVER, UNTIL MY WHOLE BLOODLINE DISAPEARS, BUT HOW CAN I MAKE THIS A REALITY ? 
-// MENTORS ALWAYS START OFF BY SAYING, "THANK THE MONEY THAT YOU USE BECAUSE IT GOES WHERE IT NEEDS TO BE" SOMETHING LIKE THAT. BUT ANYWAYS, HOW?
-// Being THANKFUL, FEEL HOW U USED TO FEEL... GOING TO WORK, EVERYDAY, EVEN THO YOU HATED IT YOU STILL DID... AND YOU WERE GOING TO SCHOOL AND YOU HAD A GIRL!! YOU WERE DOING EVERYTHING, ONLY IF..
-// I COULD START OFF FROM THERE BUT WITH ALL MY IMPROVEMENTS AND LESSONS THAT I HAVE TODAY.
-// SO I AND MONEY COULD FORMALLY GENERATE REAL WHEALTH.
-// YOU NEED A PLAN..
-
-
-
-// GRACIAS DINERO :) . Everytime you use it, no matter on what, be thankful.
-
-// Start making better decisions, first for yourself and then the group around you
-
-
-
-//This is how you loop every character in the string. 
-
-//function strr (arrrr){
-//    console.log(strr[i])
-//}
-
-
-//function array(arr){
-//return arr.split('')
-
-//}
-//console.log(array('diego'))
-
-// Foor loops 
-
-let str = ('Im good selling stuff')
-
-for (let i = 0; i < str.length; ++i){
-
-if (i % 3 === 0 && i % 5 === 0){
-    console.log(`${i} I've mastered sells`)
-}
-else if (i % 3 === 0){
-    console.log(`${i} sells`)
-}
-else if (i % 5 === 0){
-    console.log(`${i} -- > popular`)
-}
-else {
-    console.log(`${i} --> ${i}`)
+function addition(num1, num2){
+   return num1 + num2
 }
 
+console.log(addition(2,3))
+
+
+//Hours into Seconds
+
+function time(hours){
+    let seconds = (hours * 60) * 60
+    return seconds
 }
 
-let money = [20,30,40,50]
-
-money.push(200)
-
-console.log(money);
-
-
-
-
-let arr = [30,40,50,200,300]
-let newArr = arr.filter((element) => {
-    if (element < 200){
-        return true;
-    }
-})
-console.log(newArr)
-
-
-let arre = [1,2,3,4]
-let newArre = arre.map((elem3) => {
-    return 'test12313'
-})
-
-console.log(newArre);
-
-
-
-// Loop throught every word
-
-function frontendsimplified (arr){
-    for (let i = 0; i < arr.length; i++){
-        
-        console.log(frontendsimplified[i])
-    }
-    
-}
-
-
-// Si, este soy yo y me vale verga. No te da pena ser quien eres. Monitorea tu lenguaje no verbal y frame
-// Mira a los ojos derechos, conexion instanteana. 
-
-
-
-// Beginner Challenges
-
-
-// Thanks for everything, I wanna be the whole package, not just the normal one. EVERY THING... Like Ronaldo, I wanna become that guy, I need to excell that energy. Need to focus on my voice volume, think before speaking & improving even more my physic... Tired of these niggas downplaying my shit. I deserve it and Im going to get it. Amen. 
-
-// Jesus is KING but I still like my belives > : ) 
-
-// One way to improve your relationships are GIVING; give what? 
-// Give Time, Confort, Attention, Space, non tangible things//stuff
-// And of course, money too! Besides those points
-// Learn how to read what the other person wants ;) 
-
-// Thnaks Gerry
-
-function paula (num11,num22){
-return num11 + num22
-}
-
-console.log(paula(2,2))
-
-//Now, exercise your mouth, move it around and relax... Just relax and talk your mind
-// Im thankful that Im a photogenic person, no matter what angle, what haircut, what face, Im photogenic either way.
-
-
-//What's the equation behind this Mensa IQ Test... Look for patterns and problem solving. 
-// Thnkful. Gracias, Grazie
-
-
-
-//Alright!!! We getting back at it. Lets see how it goes and how u smart u get by doing all this :) 
-
-//Back at it
-
-
-// Re-learning 
+console.log(time(2))
