1cbd3d6c306f1078131c39a1c99756e77063a584
Diego <diego.minchala@outlook.com>
2023-02-26 20:56:27 -0700

    dam.ad

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ function str (arr){
 console.log(str('Karim'))
 
 
-// 
+// Array into 0's
+
+function strings (num1){
+for (let i = 0; i < num1.length; ++i){
+    num1[i] = 0
+}
+return num1; 
+}
+
+console.log(strings([12, 200])) 
