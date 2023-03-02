07119deb5cfc2b645184067f4ad5b83ad7cc1dc1
Diego <diego.minchala@outlook.com>
2023-03-02 02:11:22 -0700

    woa

--- app.js
+++ app.js
@@ -1,58 +1,11 @@
-// Reversed String
+// Bananas 
 
 function str (arr){
- return arr.split('').reverse().join('')
-}
-console.log(str('Karim'))
-
-
-// Array into 0's
-
-function strings (num1){
-for (let i = 0; i < num1.length; ++i){
-    num1[i] = 0
-}
-return num1; 
-}
-
-console.log(strings([12, 200])) 
-
-// Arrys to 0's
-
-function wtv (newarr){
-    return new Array(newarr.length).fill(0)
-}
-
-console.log(wtv([12,12323,23212,4]))
-
-// Maps
-
-function dotmap(ArrE){
-return ArrE.map(elem => {
-return 'karim'
-})
-}
-
-console.log(dotmap([1,2,23,1]))
-
-// Filter out all the apples
-
-// !== MEANS IT'S NOT!!!
-
-function smartnigga(money){
-let intelectual = []
-    for (let i =0; i < money.length; ++i){
-if (money[i] !== 'apples'){
-    intelectual.push(money[i])
-}
+    for(let i = 0; i < arr.length; ++i){
+       if (arr[i] !== 'apple'){
+        console.log(arr[i])
+       }
     }
-    return intelectual
-
 }
 
-console.log(smartnigga(['banana' , 'apples' , 'money' , 'genius']))
-
-
-// Thnaks because im smart
-
-// Los Diablooh
+console.log(str(['apple' , 'orange' , 'banana']))
