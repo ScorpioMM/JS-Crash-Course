51cb05d7dec92f52c459e6c7971b63c4dd5d7396
Diego <diego.minchala@outlook.com>
2023-03-02 02:14:59 -0700

    vacan

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Bananas 
 
 function str (arr){
+    let noApples = []
     for(let i = 0; i < arr.length; ++i){
        if (arr[i] !== 'apple'){
-        console.log(arr[i])
+        noApples.push(arr[i])
        }
     }
+    return noApples;
 }
 
 console.log(str(['apple' , 'orange' , 'banana']))
