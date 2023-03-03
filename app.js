c303644c5b28957cec2db378e9fe4661e0b60b31
Diego <diego.minchala@outlook.com>
2023-03-03 16:53:46 -0700

    i want it, i got it

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ function str (arr){
 return max;
 }
 console.log(str([2,2,2,5, 100]))
+
+
+
+
+function string (elem1, elem2) {
+    return !elem1 ? elem1 : elem2
+}
+
+console.log(string(0,500))
+
+
+function iphone (elem3){
+    return elem3.length;
+}
+
+console.log(iphone([4,4,4,8,1,2,1]))
