aa2903c96c06ff97a98047bd8465d968fc6271e5
Diego <diego.minchala@outlook.com>
2023-03-03 11:25:22 -0700

    sum

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 function str (arr){
-return arr[2]
+let sum = 0
+for (let i = 0; i < arr.length; ++i){
+    sum = sum + arr[i]
 }
-console.log(str([2,2,4]))
+return sum;
+}
+console.log(str([2,2,4,2,2]))
