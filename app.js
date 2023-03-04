174957e4d7e4b1e8b3293387c8407cf725f112d1
Diego <diego.minchala@outlook.com>
2023-03-04 08:06:01 -0700

    brr

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 function str (arr){
     let max = arr[0]
     for (let i = 0; i < arr.length; ++i){
-  if (arr[i] > max){
+if (arr[i] > max){
     max = arr[i]
-  }
+}
 }
 return max;
 }
-console.log(str([2,2,2,5, 100]))
 
-
-
-
-function string (elem1, elem2) {
-    return !elem1 ? elem1 : elem2
-}
-
-console.log(string(0,500))
-
-
-function iphone (elem3){
-    return elem3.length;
-}
-
-console.log(iphone([4,4,4,8,1,2,1]))
+console.log(str([3, 10]))
