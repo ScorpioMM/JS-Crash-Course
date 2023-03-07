b03b312b52afe4930db3dc11ba4346fd1fecbde6
Diego <diego.minchala@outlook.com>
2023-03-06 19:40:15 -0700

    :)

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-function str (arr){
-return arr.filter(elem => elem != 'apple')
-}
-
-console.log(str(['banana' , 'apple' , 'orange']))
-
 // != IF ITS NOT!!!
 
 // We create a function as normal, as soon as u read filter using the filter method should already to come your mind.
 
 // We return, the str.filter (elem =>(IF) elem != 'banana' )
+
+// If using => AND BRAKETS, you need to return...
+
+// If not, its cool. 
+
+function str (arr){
+return arr.filter(elem => elem !== 'apple' )
+}
+
+console.log(str(['apple' , 'banana' , 'pears'])) 
