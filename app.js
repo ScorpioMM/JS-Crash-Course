a796769c36c73511fe2c8ca8f9f8911051675919
Diego <diego.minchala@outlook.com>
2023-03-08 19:00:44 -0700

    ahhh

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-function str (elem1, elem2){
-return !elem1 ? elem2 : elem1
+function str (arr){
+let sum = 0;
+for (let i = 0; i <= arr; ++i){
+    sum = sum + i
+}
+return sum
 }
 
-console.log(str(true, 'dog'))
-
-
-
-function str (elem3, elem4){
- if (!elem3){
-    return elem3
- }
- else {
-    return elem4
- }
-}
-
-console.log(str(0, 500))
-
-function kaa (arre){
-    return arre.length;
-}
-
-console.log(kaa([20,1,1,1,1,1]))
+console.log(str([4]))
