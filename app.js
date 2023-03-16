b51bcf3df7c7269c33f03789a770972d977e872c
Diego <diego.minchala@outlook.com>
2023-03-16 07:26:33 -0600

    improve your memory

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ console.log(str([4,4]))
 
 
 //Improve your memory. Using sounds to re wire your brain
+
+
+function str1 (elem1){
+let sum = 0;
+for (let i = 0; i < elem1.length; ++i){
+    sum = sum + elem1[i]
+}
+return sum;
+}
+
+console.log(str1([2,2,2,2]))
