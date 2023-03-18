496776ac55e6af25605942472a3bbb26e78f8970
Diego <diego.minchala@outlook.com>
2023-03-17 19:26:46 -0600

    Memory

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 function str (arr){
-return new Array (arr.length).fill(0);
+    let max = arr[0];
+    for (let i = 0; i < arr.length; ++i){
+        if (arr[i] > max){
+            max = arr[i]
+        }
+    }
+    return max;
+
 }
 
-console.log(str([4,4]))
-
-
-// HARA YOGA
-
-function str1 (elem1){
-let sum = 0;
-for (let i = 1; i <= elem1; ++i){
-sum = sum + i
-}
-return sum;
-}
-
-console.log(str1([4]))
+console.log(str([20,100,20]))
