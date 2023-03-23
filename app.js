46954dabc4558ee4477f7328c0f4f3890427c331
Diego <diego.minchala@outlook.com>
2023-03-22 20:19:20 -0600

    hell yuhh

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 function reversedString (arr){
- let reversedString = ''
- for (let i = arr.length - 1; i >= 0; --i){
-   reversedString = reversedString + arr[i]
+ let reversedString = '';
+ for(let i = arr.length + -1; i >= 0 ; --i){
+    reversedString = reversedString + arr[i]
  }
-return reversedString;
+ return reversedString
 }
 
 console.log(reversedString('abc'))
+
+
+function str (karim){
+    let sum = 0;
+    for (let i = 0; i < karim.length; ++i){
+    sum = sum + karim[i]
+    }
+    return sum;
+}
+
+console.log(str([2,2,2]))
