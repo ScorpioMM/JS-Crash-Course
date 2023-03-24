a4d99d3ba35f8b13124a7609aaa7c0c5e92a4442
Diego <diego.minchala@outlook.com>
2023-03-24 15:25:25 -0600

    mutual benefit

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-function str (elem1){
-    let max = elem1[0]
-    for (let i = 0; i < elem1.length; ++i){
-        if(elem1[i] > max){
-            max = elem1[i]
-        }
-    }
-    return max;
+function str (arr){
+    return arr.map(elem => 0)
 }
 
-console.log(str([5,100,200,500]))
+console.log(str([3,2,1]))
