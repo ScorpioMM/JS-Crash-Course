139be6b1bec0897ea65fe097188ed81bdc00853a
Diego <diego.minchala@outlook.com>
2023-03-25 10:14:21 -0600

    Lol

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 function str (arr){
-return arr.map(elem => 0)
+let noApples = []
+for (let i = 0; i < arr.length; ++i){
+    if (arr[i] !== 'Apples'){
+        noApples.push(arr[i])
+    }
+}
+return noApples
 }
 
-console.log(str([1,1,1]))
+console.log(str(['Banana' , 'Apples' , 'Orange']))
