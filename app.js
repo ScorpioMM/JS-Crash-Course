9da724dbbd6d32bc40c494eed7c094da7afa6e23
Diego <diego.minchala@outlook.com>
2023-03-24 20:54:52 -0600

    word

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 function str (arr){
-    let str = []
-    for (let i = 0; i < arr.length; ++i){
-        if (arr[i] !== 'Apples'){
-           str.push(arr[i])
-        }
-    }
-    return str
+let str = ''
+for (let i = 0; i < arr.length;++i){
+    str = arr[i] + str
+}
+return str
 }
 
-console.log(str(['Apples' , 'Banana']))
+console.log(str('abc'))
