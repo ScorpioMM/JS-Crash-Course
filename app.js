8a86cf10a1ab17b86d543f8e5db00b9bc159d529
Diego <diego.minchala@outlook.com>
2023-03-25 19:01:01 -0600

    uh hum

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 function str (arr){
-return arr.split('').reverse().join('');
+let str = []
+for (let i = 0; i < arr.length; ++i){
+if (arr[i] !== 'Apples'){
+    str.push(arr[i])
+}
+}
+return str
 }
 
-console.log(str('abc'))
+console.log(str(['Apples', 'Guineos' , 'Peras']))
