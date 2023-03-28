ec261e19fcbed0e70b6615b7c8a64db76ebd6e14
Diego <diego.minchala@outlook.com>
2023-03-27 23:07:18 -0600

    gang

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function str (elem1){
-return new Array(elem1.length).fill(0)
+function str (arr){
+let max = arr[0]
+for (let i = 0; i < arr.length; ++i){
+if (arr[i] > max){
+max = arr[i]
+}
+}
+return max;
 }
 
-console.log(str([5, 10, 3, 10]))
+console.log(str([5, 10, 3, 101]))
