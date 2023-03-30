e26e64a45c0a4a29ed8ff00f4238540559e91b07
Diego <diego.minchala@outlook.com>
2023-03-30 14:26:46 -0600

    uhm

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-function str (arr){
-return arr.map(elem => 0)
+function showRating (rating){
+    let ratings = ""
+    for (let i = 0; i < Math.floor(rating); ++i){
+        ratings = ratings + "* "
+        if(i !== Math.floor(rating) - 1){
+            ratings = ratings + ""
+        }
+    }
+    return ratings;
 }
 
-console.log(str([5, 10, 3, 101]))
+console.log(showRating(4))
