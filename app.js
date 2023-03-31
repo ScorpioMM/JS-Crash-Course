08991eff255f46a729dad508203320a78224a084
Diego <diego.minchala@outlook.com>
2023-03-31 09:48:58 -0600

    push c

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-function lowToHigh (arr){
+// function lowToHigh (arr){
     
-    return arr.sort((a, b) => a - b)
+//     return arr.sort((a, b) => a.price - b.price)
+// }
+
+// console.log(lowToHigh([
+// {id : 1, price: 50},
+// {
+// id : 2, price: 0
+// },
+// {
+// id : 3, price: 5000
+// }
+//   ]));
+
+//   {
+//     function str (arr){
+//        return arr.length;
+//     }
+//   }
+
+//   console.log(str([2,2,2]))
+
+function str (arr) {
+let str = ""
+for(let i = 0; i < arr.length; ++i){
+    str = arr[i] + str
+}
+return str;
 }
 
-console.log(lowToHigh([20,50,100,40]))
+console.log(str('abc'))
