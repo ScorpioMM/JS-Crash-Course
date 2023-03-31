633aa0bdf25600eb59ead990bfc95b9b700f3409
Diego <diego.minchala@outlook.com>
2023-03-30 20:56:47 -0600

    brrr

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function showRatings (rating){
     let ratings = ""
-    for (let i = 0; i < Math.floor(ratings); ++i){
-        ratings += "*"
-        if (i !== ratings.isInteger(ratings)){
-
-        }
-if(!Number.isInteger(rating) )
+  for (let i = 0; i < Math.floor(rating); ++i){
+ratings += " *"
+if (i !== Math.floor(rating)){
+ratings += " "
+}
+  }
+  if (!Number.isInteger(rating)){
 ratings += " ."
-
-    }
-    return ratings;
+  }
+  return ratings
 }
 
-console.log(str([4,2]))
+console.log(showRatings(4.5))
