fba30868ed24456cb96e06c824105bad8c6d17f5
Diego <diego.minchala@outlook.com>
2023-04-01 14:17:46 -0600

    isInteger means numero entero,

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-// 1. then
+// // 1. then
 
-fetch("LINK").then(response => {
-    response.json().then ((data) => {
-        console.log(data)
-    })
-})
+// fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+//     response.json().then ((data) => {
+//         console.log(data)
+//     })
+// })
+
+function str (rating){
+let ratings = ""
+for (let i = 0; i < Math.floor(rating); ++i){
+    ratings += "*"
+    if (i !== Math.floor(rating) - 1){
+        ratings += " "
+    }
+}
+    if (!Number.isInteger(rating)){
+        ratings += " ."
+    }
+return ratings
+}
+
+console.log(str(4.5))
