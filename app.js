0c5113f4e7a89c3eefeeb61f2ad5ab28e1c326a5
Diego <diego.minchala@outlook.com>
2023-04-01 11:24:26 -0600

    brr

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-//Theres two ways to unlock a backend data, both are important
+// 1. then
 
-const emailRef = document.querySelector(".email")
-
-// 1. Then
-
-
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-response.json().then((data) => {
-    console.log(data)
+fetch("LINK").then(response => {
+    response.json().then ((data) => {
+        console.log(data)
+    })
 })
-})
-
-
-
-// 2. Async Awaitttt
-
-
-// async function main (){
-// const response = await fetch ("https://jsonplaceholder.typicode.com/users/1")
-// const data = await response.json()
-
-// console.log(data);
-// emailRef.innerHTML = data.email
-// }
-
-// main()
