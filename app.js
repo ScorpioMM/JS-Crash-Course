4e2449959d82ef2583796910973da98aa5a7ad41
Diego <diego.minchala@outlook.com>
2023-04-01 00:23:31 -0600

    word

--- app.js
+++ app.js
@@ -2,28 +2,26 @@
 
 const emailRef = document.querySelector(".email")
 
+// 1. Then
 
 
-// 1. Then 
-// console.log(emailRef)
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+response.json().then((data) => {
+    console.log(data)
+})
+})
 
-// fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-//  return response.json()
-// }).then(data => {
-//     console.log(data);
-//     emailRef.innerHTML = data.email
-// })
 
 
 // 2. Async Awaitttt
 
 
-async function main (){
-const response = await fetch ("https://jsonplaceholder.typicode.com/users/1")
-const data = await response.json()
+// async function main (){
+// const response = await fetch ("https://jsonplaceholder.typicode.com/users/1")
+// const data = await response.json()
 
-console.log(data);
-emailRef.innerHTML = data.email
-}
+// console.log(data);
+// emailRef.innerHTML = data.email
+// }
 
-main()
+// main()
