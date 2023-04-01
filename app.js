58c3ce17cfec7ad2059413723a868c93a0a35f7d
Diego <diego.minchala@outlook.com>
2023-03-31 22:45:15 -0600

    async await = response.json

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 //Theres two ways to unlock a backend data, both are important
 
 const emailRef = document.querySelector(".email")
-console.log(emailRef)
-// 1. Then 
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
- return response.json()
-}).then(data => {
-    console.log(data);
-    emailRef.innerHTML = data.email
-})
+
+
+// 1. Then 
+// console.log(emailRef)
+
+// fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+//  return response.json()
+// }).then(data => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email
+// })
 
 
 // 2. Async Awaitttt
+
+
+async function main (){
+const response = await fetch ("https://jsonplaceholder.typicode.com/users/1")
+const data = await response.json()
+
+console.log(data);
+emailRef.innerHTML = data.email
+}
+
+main()
