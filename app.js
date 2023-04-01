70a8e5a26562e1ea2923a0eda76cf602e50d2d79
Diego <diego.minchala@outlook.com>
2023-04-01 15:29:45 -0600

    br

--- app.js
+++ app.js
@@ -6,18 +6,36 @@
 //     })
 // })
 
-function str (rating){
-let ratings = ""
-for (let i = 0; i < Math.floor(rating); ++i){
-    ratings += "*"
-    if (i !== Math.floor(rating) - 1){
-        ratings += " "
-    }
-}
-    if (!Number.isInteger(rating)){
-        ratings += " ."
-    }
-return ratings
+// function str (rating){
+// let ratings = ""
+// for (let i = 0; i < Math.floor(rating); ++i){
+//     ratings += "*"
+//     if (i !== Math.floor(rating) - 1){
+//         ratings += " "
+//     }
+// }
+//     if (!Number.isInteger(rating)){
+//         ratings += " ."
+//     }
+// return ratings
+// }
+
+// console.log(str(3.5))
+
+
+function sortToLowToHigh (numbers){
+return numbers.sort((a, b) => a.price - b.price)
+
 }
 
-console.log(str(4.5))
+console.log(sortToLowToHigh([
+    {
+id : 1 , price: 50
+    } , 
+    {
+        id : 2 , price: 200
+    } , 
+    {
+        id : 3 , price: 40
+    }
+]))
