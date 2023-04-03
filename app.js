f75fe3168f2a818145a3cbd47092df1dad7c2faf
Diego <diego.minchala@outlook.com>
2023-04-03 13:05:27 -0600

    gang

--- app.js
+++ app.js
@@ -4,25 +4,41 @@ const emailRef = document.querySelector(".email")
 
 // 1. Then 
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
-return response.json();    
-}).then((data) => {
-    console.log(data)
-    emailRef.innerHTML = data.email
-})
-
+// fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
+//     return response.json()
+// }).then((data) => {
+//     console.log(data)
+// emailRef.innerHTML = data.email
+// })
 // 2. Await/Async 
 
+async function main(){
+    
+    await fetch ("https://jsonplaceholder.typicode.com/users/1")
 
 
-// Reverse words
-
-function str (arr){
-let str = []
-for (let i = 0; i < arr.length; ++i){
-    str = arr[i] + str
-}
-return str;    
 }
 
-console.log(str('abc'))
+main()
+
+
+
+
+
+
+
+
+
+
+
+// // Reverse words
+
+// function str (arr){
+// let str = []
+// for (let i = 0; i < arr.length; ++i){
+//     str = arr[i] + str
+// }
+// return str;    
+// }
+
+// console.log(str('abc'))
