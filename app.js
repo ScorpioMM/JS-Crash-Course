14c2c210ff4fb4eead21baf3f9cb131450fb4c65
Diego <diego.minchala@outlook.com>
2023-04-02 23:06:38 -0600

    '

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
-// // 1. then
+// InnerHTML
 
-// fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-//     response.json().then ((data) => {
-//         console.log(data)
-//     })
-// })
+const emailRef = document.querySelector(".email")
 
-// function str (rating){
-// let ratings = ""
-// for (let i = 0; i < Math.floor(rating); ++i){
-//     ratings += "*"
-//     if (i !== Math.floor(rating) - 1){
-//         ratings += " "
-//     }
-// }
-//     if (!Number.isInteger(rating)){
-//         ratings += " ."
-//     }
-// return ratings
-// }
+// 1. Then 
 
-// console.log(str(3.5))
-
-
-function sortToLowToHigh (numbers){
-return numbers.sort((a, b) => a.price - b.price)
-
-}
-
-console.log(sortToLowToHigh([
-    {
-id : 1 , price: 50
-    } , 
-    {
-        id : 2 , price: 200
-    } , 
-    {
-        id : 3 , price: 40
-    }
-]))
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+    response.json().then((data) => 
+    console.log(data))
+})
