3522fbb4f1ead0f66968ee53a137e185bcd78baa
Diego <diego.minchala@outlook.com>
2023-04-02 23:16:48 -0600

    lol

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// InnerHTML
+// InnerHTMLLLL
 
 const emailRef = document.querySelector(".email")
 
@@ -8,3 +8,29 @@ fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
     response.json().then((data) => 
     console.log(data))
 })
+
+
+// 2. Await/Async 
+
+
+
+// Sum 
+
+function str (arr){
+let sum = 0
+for (let i = 0; i < arr.lenght; ++i){
+    sum = sum + arr[i]
+}
+return sum;
+}
+console.log(str([2,2,2]))
+
+
+// Proggresive sum
+
+function strr (arrr){
+let sum = 0
+
+}
+
+console.log(strr([2]))
