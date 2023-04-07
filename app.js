9eb276e09b184f314213ac6d5b21592bb8b25c41
Diego <diego.minchala@outlook.com>
2023-04-06 23:29:03 -0600

    dam

---
+++ app.js
@@ -0,0 +1,21 @@
+// template_03yppsl
+
+// service_3sp6jop
+
+
+//x8MQHtCs_-2lY1q5o
+
+
+function contact(event) {
+    event.preventDefault();
+emailJs
+.sendForm(
+    'service_3sp6jop' ,
+    'template_03yppsl' ,
+    event.target ,
+    'x8MQHtCs_-2lY1q5o'
+).then(() => {
+    console.log('this worked')
+})
+
+}
