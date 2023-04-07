2339f223569ca2552a2174c25b0a755f0de76dfc
Diego <diego.minchala@outlook.com>
2023-04-07 13:57:06 -0600

    asd

--- app.js
+++ app.js
@@ -8,16 +8,13 @@
 
 function contact(event) {
     event.preventDefault();
-// emailJs
-// .sendForm(
-//     'service_3sp6jop' ,
-//     'template_03yppsl' ,
-//     event.target ,
-//     'x8MQHtCs_-2lY1q5o'
-// ).then(() => {
-//     console.log('this worked')
-// })
+const loading = document.querySelector('.modal_overlay-loading');
+const success = document.querySelector('.modal_overlay-success');
+loading.classList += " modal_overlay-visible"; 
+
 setTimeout(() => {
-    console.log('it worked1' )
-} , 500)
+    loading.classList.remove("modal_overlay-visible");
+    success.classList  +=  " modal_overlay-visible";
+    console.log('it worked 1' )
+} , 1000)
 }
