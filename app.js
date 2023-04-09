1a1c0f50dce54167eb1b69f5ffcdb064826e40f1
Diego <diego.minchala@outlook.com>
2023-04-08 23:22:26 -0600

    as

--- app.js
+++ app.js
@@ -2,19 +2,5 @@
 
 // service_3sp6jop
 
-
 //x8MQHtCs_-2lY1q5o
 
-
-function contact(event) {
-    event.preventDefault();
-const loading = document.querySelector('.modal_overlay-loading');
-const success = document.querySelector('.modal_overlay-success');
-loading.classList += " modal_overlay-visible"; 
-
-setTimeout(() => {
-    loading.classList.remove("modal_overlay-visible");
-    success.classList  +=  " modal_overlay-visible";
-    console.log('it worked 1' )
-} , 1000)
-}
